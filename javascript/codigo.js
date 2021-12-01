5ced2ce9bf7ec4c2d85abe33802e8be3bbef8aba
forgegabriel <forgegabriel@gmail.com>
2021-12-01 15:25:18 -0300

    Adicionado arquivo js e trecho 3

---
+++ javascript/codigo.js
@@ -0,0 +1,15 @@
+function t3p1paragrafoInnerHtml() {
+    document.getElementById("t3p1-muda-paragrafo").innerHTML = 3 + 4;
+}
+
+function t3p3alerta() {
+    alert(3 + 4);
+}
+
+function t3p4consoleLog() {
+    console.log(3 + 4);
+}
+
+function t3p5print() {
+    window.print();
+}
